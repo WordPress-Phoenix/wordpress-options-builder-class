2409300c71574b3709f441517bff621ca5c7a4c1
David Ryan <0aveRyan@users.noreply.github.com>
2018-05-07 16:02:12 -0700

    Update wpop-init.php

--- wpop-init.php
+++ wpop-init.php
@@ -4,7 +4,7 @@
  *
  * @authors 🌵 WordPress Phoenix 🌵 / Seth Carstens, David Ryan
  * @package wpop
- * @version 3.1.0
+ * @version 4.1.1
  * @license GPL-2.0+ - please retain comments that express original build of this file by the author.
  */
 
