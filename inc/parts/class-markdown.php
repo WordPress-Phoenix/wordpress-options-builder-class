32bd0d06cb1a87ba3c6f2a587abf0ac5f6fe4bbf
Seth Carstens <seth.carstens@gmail.com>
2018-05-11 10:04:52 -0700

    updates requested by VIP + some phpcs rules and some relative fixes

--- inc/parts/class-markdown.php
+++ inc/parts/class-markdown.php
@@ -1,21 +1,39 @@
 <?php
+/**
+ * Markdown
+ *
+ * @package    WordPress
+ * @subpackage WPOP
+ */
 
 namespace WPOP\V_4_1;
 
 /**
  * Class Markdown
+ *
  * @package WPOP\V_3_1
  */
 class Markdown extends Include_Partial {
+	/**
+	 * Field Type
+	 *
+	 * @var string
+	 */
 	public $field_type = 'markdown_file';
 
+	/**
+	 * Main render function for the field.
+	 *
+	 * @return string
+	 */
 	public function render() {
 		if ( is_file( $this->filename ) && class_exists( '\\Parsedown' ) ) {
 			$converter = new \Parsedown();
-			$markup    = file_get_contents( $this->filename );
+			$markup    = static::wpvip_safe_get_contents( $this->filename );
 			if ( ! empty( $markup ) ) {
-				// TODO: fix markdown field
+				return 'To do, fix markdown field.';
 			}
+			return 'Markup was empty.';
 		} else {
 			return 'File Status: ' . strval( is_file( $this->filename ) ) . ' and class exists: ' . strval( class_exists( '\\Parsedown' ) );
 		}
