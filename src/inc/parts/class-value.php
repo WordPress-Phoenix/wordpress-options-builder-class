80cac75fd26b86ab8d733a61c49fe4f51be8e33a
Chris Van Patten <chris@vanpattenmedia.com>
2019-02-07 18:59:48 -0500

    Avoid redefining the constructor when we don't fundamentally change it

--- src/inc/parts/class-value.php
+++ src/inc/parts/class-value.php
@@ -14,6 +14,7 @@ namespace WPOP\V_5_0;
  * @package WPOP\V_5_0
  */
 class Value extends Part {
+
 	/**
 	 * String value
 	 *
@@ -29,20 +30,10 @@ class Value extends Part {
 	public $input_type = 'value';
 
 	/**
-	 * Include_Partial constructor.
-	 *
-	 * @param string $i      Slug or ID.
-	 * @param array  $config Configuration arguments for customizing object instance.
-	 */
-	public function __construct( $i, $config ) {
-		parent::__construct( $i, [] );
-		$this->value = ( ! empty( $config['value'] ) ) ? $config['value'] : '';
-	}
-
-	/**
 	 * Main render function.
 	 */
 	public function render() {
+		var_dump($this);die;
 		// Guard clause to return early if we don't have a value.
 		if ( empty( $this->value ) ) {
 			return;
@@ -51,7 +42,7 @@ class Value extends Part {
 		echo sprintf(
 			'<li class="%1$s">%2$s</li>',
 			esc_attr( $this->get_clean_classname() ),
-			wp_kses( $this->string, wp_kses_allowed_html() )
+			wp_kses( $this->value, wp_kses_allowed_html() )
 		);
 	}
 
