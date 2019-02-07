9e73dcf6927e2fba553d4011d9bbbc68038dcb5f
Chris Van Patten <chris@vanpattenmedia.com>
2019-02-07 12:55:18 -0500

    Leverage sprintf for rendering HTML

--- src/inc/parts/class-textarea.php
+++ src/inc/parts/class-textarea.php
@@ -47,14 +47,14 @@ class Textarea extends Part {
 	public function render() {
 		$this->cols = ! empty( $this->cols ) ? $this->cols : 80;
 		$this->rows = ! empty( $this->rows ) ? $this->rows : 10;
-		// @codingStandardsIgnoreStart
-		?><textarea title="<?php echo esc_attr( $this->id ); ?>"
-                    id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>"
-                    cols="<?php echo esc_attr( $this->cols ); ?>" rows="<?php echo esc_attr( $this->rows ); ?>"><?php
-		echo esc_html( $this->get_saved() );
-		?></textarea>
-		<?php
-		// @codingStandardsIgnoreEnd
+
+		echo sprintf(
+			'<textarea title="%1$s" id="%1$s" name="%1$s" cols="%2$d" rows="%3$d">%4$s</textarea>',
+			esc_attr( $this->id ),
+			esc_attr( $this->cols ),
+			esc_attr( $this->rows ),
+			esc_html( $this->get_saved() )
+		);
 	}
 
 }
