d515ed7a08576dc89f440f80f321e4c87491a52d
Chris Van Patten <chris@vanpattenmedia.com>
2019-05-13 15:06:19 -0400

    Update partial to use a traditional `include`

--- src/inc/parts/class-include-partial.php
+++ src/inc/parts/class-include-partial.php
@@ -36,37 +36,23 @@ class Include_Partial extends Part {
 	 */
 	public function __construct( $i, $config ) {
 		parent::__construct( $i, [] );
-		$this->filename = ( ! empty( $config['filename'] ) ) ? $config['filename'] : 'set_the_filename.php';
+
+		$this->filename = ! empty( $config['filename'] ) ? $config['filename'] : '';
 	}
 
 	/**
 	 * Main render function.
 	 */
 	public function render() {
-		if ( ! empty( $this->filename ) && is_file( $this->filename ) ) { ?>
-			<li class="<?php echo esc_attr( $this->get_clean_classname() ); ?>">
-				<?php
-				if ( function_exists( 'wpcom_vip_file_get_contents' ) ) {
-					$contents = wpcom_vip_file_get_contents( $this->filename );
-				} else {
-					/**
-					 * We need to ignore this file_get_contents call, because it's
-					 * required outside of a WordPress VIP environment (where the
-					 * safer `wpcom_vip_file_get_contents()` is available).
-					 *
-					 * In a VIP environment, this will never be called.
-					 */
-
-					// phpcs:disable
-					$contents = file_get_contents( $this->filename );
-					// phpcs:enable
-				}
-
-				echo wp_kses( $contents, wp_kses_allowed_html() );
-				?>
-			</li>
-			<?php
+		if ( empty( $this->filename ) || ! is_file( $this->filename ) ) {
+			return;
 		}
+
+		?>
+		<li class="<?php echo esc_attr( $this->get_clean_classname() ); ?>">
+			<?php include $this->filename; ?>
+		</li>
+		<?php
 	}
 
 }
